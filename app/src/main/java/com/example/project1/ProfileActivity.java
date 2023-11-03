97bd56d857a02c2b491d87bae79cd380157745c3
Kaveesha-Kumaradasa <116972496+Kaveesha-Kumaradasa@users.noreply.github.com>
2023-11-03 19:15:39 +0530

    First

--- app/src/main/java/com/example/project1/ProfileActivity.java
+++ app/src/main/java/com/example/project1/ProfileActivity.java
@@ -4,12 +4,23 @@ import static com.example.project1.R.id.profile;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class ProfileActivity extends AppCompatActivity {
+    private boolean userIsLoggedIn = true; // Replace with your authentication logic
+
+    private TextView loginMessage;
+    private LinearLayout profileDetailsLayout;
+    private TextView nameTextView;
+    private TextView emailTextView;
+    private Button logoutButton;
 
     BottomNavigationView bottomNavigationView;
 
@@ -20,6 +31,37 @@ public class ProfileActivity extends AppCompatActivity {
 
         bottomNavigationView = findViewById(R.id.bottom_nav);
         bottomNavigationView.setSelectedItemId(profile);
+
+        loginMessage = findViewById(R.id.loginMessage);
+        profileDetailsLayout = findViewById(R.id.profileDetailsLayout);
+        nameTextView = findViewById(R.id.nameTextView);
+        emailTextView = findViewById(R.id.emailTextView);
+        logoutButton = findViewById(R.id.logoutButton);
+
+        if (userIsLoggedIn) {
+            loginMessage.setVisibility(View.GONE);
+            profileDetailsLayout.setVisibility(View.VISIBLE);
+
+            // Replace with actual user data
+            String name = "John Doe";
+            String email = "john@example.com";
+
+            nameTextView.setText("Name: " + name);
+            emailTextView.setText("Email: " + email);
+
+            // Add more user details as needed
+
+            logoutButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    openLoginActivity();
+                    // Handle logout and navigate to login screen
+                }
+            });
+        } else {
+            loginMessage.setVisibility(View.VISIBLE);
+            profileDetailsLayout.setVisibility(View.GONE);
+        }
 
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
@@ -40,5 +82,11 @@ public class ProfileActivity extends AppCompatActivity {
                 return false;
             }
         });
+    }
+
+    private void openLoginActivity() {
+
+        Intent intent = new Intent(this, loginActivity.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/example/project1/loginActivity.java
+++ app/src/main/java/com/example/project1/loginActivity.java
@@ -1,14 +1,22 @@
 package com.example.project1;
 
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class loginActivity extends AppCompatActivity {
@@ -16,6 +24,7 @@ public class loginActivity extends AppCompatActivity {
     private EditText Email, Password;
     private Button Loginbtn;
     private FirebaseAuth firebaseAuth;
+    private TextView ForgotPassword;
 
 
     @Override
@@ -26,6 +35,7 @@ public class loginActivity extends AppCompatActivity {
         Email = findViewById(R.id.email);
         Password = findViewById(R.id.password);
         Loginbtn = findViewById(R.id.loginbtn);
+        ForgotPassword = findViewById(R.id.forgotpassword);
 
         firebaseAuth = FirebaseAuth.getInstance();
 
@@ -69,6 +79,53 @@ public class loginActivity extends AppCompatActivity {
     public void openSignupActivity() {
         Intent intent = new Intent(this, SignupActivity.class);
         startActivity(intent);
+
+        ForgotPassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(loginActivity.this);
+                View dialogView = getLayoutInflater().inflate(R.layout.dialog_forget,null);
+                EditText emailBox = dialogView.findViewById(R.id.emailbox);
+
+                builder.setView(dialogView);
+                AlertDialog dialog = builder.create();
+
+                dialogView.findViewById(R.id.btnReset).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String userEmail = emailBox.getText().toString();
+
+                        if (TextUtils.isEmpty(userEmail) && !Patterns.EMAIL_ADDRESS.matcher(userEmail).matches()){
+                            Toast.makeText(loginActivity.this, "Enter your registered email id", Toast.LENGTH_SHORT).show();
+                            return;
+                        }
+
+                        firebaseAuth.sendPasswordResetEmail(userEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()){
+                                    Toast.makeText(loginActivity.this, "Check your email", Toast.LENGTH_SHORT).show();
+                                    dialog.dismiss();
+                                } else {
+                                    Toast.makeText(loginActivity.this, "Unable to send, Failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                });
+
+                dialogView.findViewById(R.id.btncancle).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        dialog.dismiss();
+                    }
+                });
+                if (dialog.getWindow() != null){
+                    dialog.getWindow().setBackgroundDrawable(new ColorDrawable(0));
+                }
+                dialog.show();
+            }
+        });
 
     }
 }
